912b10488c4c1994aef847f1a2ceadbcffd12a10
Erkki Lindpere <erkki.lindpere@zeroturnaround.com>
2016-02-07 17:44:56 +0200

    Play sounds in app always

--- thebomby/app/src/main/java/com/wordpress/thebomby/device/BluetoothBomb.java
+++ thebomby/app/src/main/java/com/wordpress/thebomby/device/BluetoothBomb.java
@@ -1,10 +1,16 @@
 package com.wordpress.thebomby.device;
 
+import android.media.MediaPlayer;
+
 import com.wordpress.thebomby.GameActivity;
+import com.wordpress.thebomby.R;
 
 public class BluetoothBomb implements Bomb {
 
     private final BluetoothBombChannel channel;
+    private MediaPlayer ticker;
+    private MediaPlayer explosion;
+    private MediaPlayer skip;
 
     public BluetoothBomb() {
         this.channel = new BluetoothBombChannel();
@@ -24,6 +30,9 @@ public class BluetoothBomb implements Bomb {
     public void timerDone() {
         setBombListener(null);
         channel.sendAsyncMessage(0xD0, null);
+        skip.stop();
+        ticker.stop();
+        explosion.start();
     }
 
     @Override
@@ -34,6 +43,11 @@ public class BluetoothBomb implements Bomb {
     @Override
     public void start(GameActivity gameActivity) {
         setBombListener(gameActivity);
+        ticker = MediaPlayer.create(gameActivity, R.raw.tick1sec);
+        ticker.setLooping(true);
+        ticker.start();
+        explosion = MediaPlayer.create(gameActivity, R.raw.explode);
+        skip = MediaPlayer.create(gameActivity, R.raw.skip);
     }
 
     @Override
